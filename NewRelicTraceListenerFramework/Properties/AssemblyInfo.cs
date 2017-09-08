eaa0aef316678895b8d6e13ffd18ae8f51ae2582
Alex McCool <alex.mccool@yahoo.com>
2017-09-07 23:42:26 -0700

    for nuspec

--- NewRelicTraceListenerFramework/Properties/AssemblyInfo.cs
+++ NewRelicTraceListenerFramework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NewRelicTraceListenerFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NewRelic.TraceListener")]
+[assembly: AssemblyDescription("TraceListener for NewRelic")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NewRelicTraceListenerFramework")]
+[assembly: AssemblyProduct("NewRelic.TraceListener")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
